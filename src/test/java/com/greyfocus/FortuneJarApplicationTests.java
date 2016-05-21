059506c757f7ddad7e8b3cecc13f8f3666d9baef
Cristian Vasilache <greyfocus@users.noreply.github.com>
2016-05-22 00:36:35 +0300

    Project structure and code style changes

--- src/test/java/com/greyfocus/FortuneJarApplicationTests.java
+++ src/test/java/com/greyfocus/FortuneJarApplicationTests.java
@@ -1,13 +1,15 @@
 package com.greyfocus;
 
+import com.greyfocus.rest.RestValidationConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = QuotesApplication.class)
+@SpringBootTest(classes = QuotesApplication.class)
 @WebAppConfiguration
 public class FortuneJarApplicationTests {
 

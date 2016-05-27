bb58fdc6fe5ea2d5d9e1bf330667f8df9e09450a
Cristian Vasilache <greyfocus@users.noreply.github.com>
2016-05-27 23:17:49 +0300

    Add support for linking to individual quotes.

--- src/main/java/com/greyfocus/quotes/web/AppController.java
+++ src/main/java/com/greyfocus/quotes/web/AppController.java
@@ -1,8 +1,11 @@
 package com.greyfocus.quotes.web;
 
+import com.greyfocus.quotes.model.Quote;
 import com.greyfocus.quotes.persistence.QuoteRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.inject.Inject;
@@ -15,8 +18,24 @@ public class AppController {
 
     @RequestMapping("/")
     public String randomQuote(Model model) {
-        // TODO: error handling
-        model.addAttribute("quote", quoteRepository.randomQuote());
+        Quote randomQuote = quoteRepository.randomQuote();
+        if (randomQuote == null) {
+            throw new ResourceNotFoundException("No quote was found.");
+        }
+
+        model.addAttribute("quote", randomQuote);
+
+        return "index";
+    }
+
+    @RequestMapping("/quotes/{quoteId}")
+    public String quoteById(@PathVariable("quoteId") String quoteId, Model model) {
+        Quote quote = quoteRepository.findOne(quoteId);
+        if (quote == null) {
+            throw new ResourceNotFoundException("No quote was found.");
+        }
+
+        model.addAttribute("quote", quote);
 
         return "index";
     }
